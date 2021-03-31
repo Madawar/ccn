eac63f127a8346fbddaa98a7030856246f164128
Dennis Wanyoike <dwanyoike@codedcell.com>
2021-03-31 13:06:05 +0300

    Remove Unwanted filters
    Test

--- resources/views/livewire/releases.blade.php
+++ resources/views/livewire/releases.blade.php
@@ -1,29 +1,6 @@
 <div>
     <div class="my-2 flex sm:flex-row flex-col justify-center bg-gray-100 p-2 shadow-sm">
-        <div class="flex flex-row mb-1 sm:mb-0">
-            <div class="relative">
-                <select wire:model="pagination"
-                    class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                </select>
 
-            </div>
-
-            <div class="relative">
-                <select wire:model="filter"
-                    class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                    <option value="null">All</option>
-                    <option value="1">Finalized</option>
-                    <option value="unapproved">Unapproved</option>
-                    <option value="review">For Review By Osh Department</option>
-                    <option value="unresponsive">Assigned But No Response</option>
-                    <option value="toMe">Assigned To Me</option>
-                </select>
-
-            </div>
-        </div>
         <div class="block relative">
             <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
                 <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
@@ -33,7 +10,7 @@
                 </svg>
             </span>
             <input placeholder="Search" wire:model="search"
-                class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                class="appearance-none rounded sm:rounded-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
         </div>
 
     </div>
